cf66d280fa973ecbe7af04f905561ae0e2138d61
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2025-02-02 22:11:47 -0300

    Added migration of posts and creation of posts for every rss feed

--- scraper.go
+++ scraper.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
 	"sync"
 	"time"
 
 	"github.com/Axl-91/go-rss/internal/database"
+	"github.com/google/uuid"
 )
 
 func startScraping(db *database.Queries, concurrency int, timeBetweenRequests time.Duration) {
@@ -48,7 +50,33 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 	}
 
 	for _, item := range rssFeed.Channel.Item {
-		log.Println("Found post: ", item.Title)
+		description := sql.NullString{}
+		if item.Description != "" {
+			description.String = item.Description
+			description.Valid = true
+		}
+
+		publishedDate, err := time.Parse(time.RFC1123Z, item.PubDate)
+		if err != nil {
+			log.Printf("Error parsing the published date: %v\n", err)
+			continue
+		}
+
+		_, err =
+			db.CreatePost(context.Background(), database.CreatePostParams{
+				ID:          uuid.New(),
+				Title:       item.Title,
+				Description: description,
+				PublishedAt: publishedDate,
+				Url:         item.Link,
+				FeedID:      feed.ID,
+				CreatedAt:   time.Now(),
+				UpdatedAt:   time.Now(),
+			})
+		if err != nil {
+			log.Printf("Error creating Post: %v\n", err)
+			continue
+		}
 	}
 
 	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
